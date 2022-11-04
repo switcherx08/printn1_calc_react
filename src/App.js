1e450bde424ce3414523a718893dbb6bad692d1b
Your Name <director@printn1.ru>
2022-11-05 00:53:14 +0300

    Login Form

--- src/App.js
+++ src/App.js
@@ -4,14 +4,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from "./pages/Navbar/Navbar";
 import Calculator from "./pages/SheetCalculator/Calculation";
 import {SavedCalculations} from "./pages/SavedCalculations";
+import LoginForm from "./pages/Login";
 
 
 function App() {
+    // let y = localStorage.getItem('y')
+    // if (y === '0'){
+    //     return (<LoginForm/>)
+    // }
     return (
         <BrowserRouter>
             <Navbar/>
             <main>
                 <Routes>
+                    <Route path='/login' element={<LoginForm /> }/>
                     <Route path='/calc' element={< Calculator />}/>
                     <Route path='/saved' element={< SavedCalculations />}/>
                 </Routes>
